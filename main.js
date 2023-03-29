d473672758f3ec38d56d92125b74b31c5d95c441
jkeg892g <128487641+jkeg892g@users.noreply.github.com>
2023-03-29 18:57:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Libraries
+const packageJson = require('./package.json');
+
+console.log('  /$$$$$$  /$$ /$$                   /$$    ');
+console.log(' /$$__  $$| $$| $$                  | $$    ');
+console.log('| $$  \\__/| $$| $$$$$$$   /$$$$$$  /$$$$$$  ');
+console.log('|  $$$$$$ | $$| $$__  $$ /$$__  $$|_  $$_/  ');
+console.log(' \\____  $$| $$| $$  \\ $$| $$  \\ $$  | $$    ');
+console.log(' /$$  \\ $$| $$| $$  | $$| $$  | $$  | $$ /$$');
+console.log('|  $$$$$$/| $$| $$$$$$$/|  $$$$$$/  |  $$$$/');
+console.log(' \\______/ |__/|_______/  \\______/    \\___/  ');
+console.log('');
+console.log('Made by:', packageJson.author);
+console.log('Version:', packageJson.version);
+console.log('════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════')
